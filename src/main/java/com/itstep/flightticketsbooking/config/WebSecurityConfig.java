930f8c23a75ebb1dd36151d716926b027377830c
Max Shaptala <maxshaptala@gmail.com>
2022-12-23 20:48:22 +0200

    Updated security configuration: fixed roles

--- src/main/java/com/itstep/flightticketsbooking/config/WebSecurityConfig.java
+++ src/main/java/com/itstep/flightticketsbooking/config/WebSecurityConfig.java
@@ -3,9 +3,17 @@ package com.itstep.flightticketsbooking.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.ForwardAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 public class WebSecurityConfig {
@@ -19,11 +27,17 @@ public class WebSecurityConfig {
     public SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .authorizeHttpRequests(authorize -> authorize
-                        .mvcMatchers("/", "/h2-console/flights", "/registration").permitAll()
-                        .mvcMatchers("/adminPage/**").hasRole("ADMIN")
-                        .mvcMatchers("/userPage/**").hasRole("USER")
+                        .antMatchers("/", "/h2-console/flights", "/registration").permitAll()
+                        .antMatchers("/adminPage/**").hasRole("ADMIN")
+                        .antMatchers("/userPage/**").hasRole("USER")
                         .anyRequest().denyAll())
-                .formLogin()
+                .formLogin().successHandler((request, response, authentication) -> {
+                    if (authentication.getAuthorities().stream().anyMatch(grantedAuthority -> grantedAuthority.getAuthority().contains("ADMIN"))) {
+                        response.sendRedirect("/adminPage");
+                    } else {
+                        response.sendRedirect("/userPage");
+                    }
+                })
                 .and()
                 .build();
     }
