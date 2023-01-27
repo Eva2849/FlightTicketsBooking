5283e9e22acb4cdb86f10a158c443ddc50470030
EDward1331 <edward_wh@ukr.net>
2023-01-27 20:39:14 +0200

    Change 27.01.23

--- src/main/java/com/itstep/flightticketsbooking/api/dto/FlightDto.java
+++ src/main/java/com/itstep/flightticketsbooking/api/dto/FlightDto.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.swing.text.html.parser.Entity;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.Duration;
