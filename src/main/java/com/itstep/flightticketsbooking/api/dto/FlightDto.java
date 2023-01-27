6fbf4166a1ac2f6d06ac80e4a198a5089114c9e0
Max Shaptala <maxshaptala@gmail.com>
2023-01-27 20:25:33 +0200

    Made page to update flights

--- src/main/java/com/itstep/flightticketsbooking/api/dto/FlightDto.java
+++ src/main/java/com/itstep/flightticketsbooking/api/dto/FlightDto.java
@@ -1,14 +1,19 @@
 package com.itstep.flightticketsbooking.api.dto;
 
 import com.itstep.flightticketsbooking.entity.Flight;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.swing.text.html.parser.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FlightDto {
     public static final String TIME_REGEX = "(\\d+):(\\d+)";
     public static final String DURATION_REPLACEMENT = "PT$1H$2M";
@@ -33,5 +38,12 @@ public class FlightDto {
     public Flight toEntityFlight() {
         String duration = estFlightDuration.replaceFirst(TIME_REGEX, DURATION_REPLACEMENT);
         return new Flight(flightNumber, departureDate, origin, destination, Duration.parse(duration), maxNumSeats);
+    }
+
+    public static FlightDto fromEntity(Flight flight) {
+        Duration flightDuration = flight.getEstFlightDuration();
+        String duration = String.format("%d:%d", flightDuration.toHours(), flightDuration.toMinutesPart());
+        return new FlightDto(flight.getFlightNumber(), flight.getDepartureDate(), flight.getOrigin(),
+                flight.getDestination(), duration, flight.getMaxNumSeats());
     }
 }
--- src/main/java/com/itstep/flightticketsbooking/controller/UpdateFlightController.java
+++ src/main/java/com/itstep/flightticketsbooking/controller/UpdateFlightController.java
@@ -1,28 +1,35 @@
 package com.itstep.flightticketsbooking.controller;
 
 import com.itstep.flightticketsbooking.api.dto.ErrorResponse;
+import com.itstep.flightticketsbooking.api.dto.FlightDto;
 import com.itstep.flightticketsbooking.entity.Flight;
 import com.itstep.flightticketsbooking.entity.User;
 import com.itstep.flightticketsbooking.repository.FlightRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RequestMapping("/updateFlight")
-@RestController
 @RequiredArgsConstructor
+@Controller
 public class UpdateFlightController {
     private final FlightRepository flightRepository;
-//    @GetMapping
-//    String index(Model model) {
-//        model.addAttribute("flight", showFlight(id));
-//        return "registration";
-//    }
+
+    @GetMapping("/{id}")
+    String index(@PathVariable long id, Model model) {
+        model.addAttribute("flight", FlightDto.fromEntity(flightRepository.findById(id).orElseThrow()));
+        model.addAttribute("flightId", id);
+        return "updateFlight";
+    }
 
     @GetMapping("/flights/{id}")
+    @ResponseBody
     public ResponseEntity<?> showFlight(@PathVariable Long id) {
         Optional<Flight> optionalFlight = flightRepository.findById(id);
         if (optionalFlight.isPresent()) {
@@ -31,8 +38,15 @@ public class UpdateFlightController {
         return ResponseEntity.badRequest().body("Flight not found");
     }
 
-    @PutMapping("/flights/{id}")
-    public ResponseEntity<?> updateFlight(@PathVariable Long id, @RequestBody Flight flight) {
+    @PutMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<?> updateFlight(@PathVariable Long id, @RequestBody @Validated FlightDto flightDto, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(new ErrorResponse("Error validation: " + bindingResult));
+        }
+
+        Flight flight = flightDto.toEntityFlight();
+
         Optional<Flight> optionalFlight = flightRepository.findById(id);
         if (optionalFlight.isPresent()) {
             Flight fl = optionalFlight.get();
